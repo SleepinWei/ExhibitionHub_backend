d24bea7b043bef53f795ea9aff794c6e1362e4e5
SleepinWei <zyw0241@outlook.com>
2023-04-16 16:41:46 +0800

    feat : add get handler for exhibition info

--- src/main/java/com/exhibition/controller/ExhibitionController.java
+++ src/main/java/com/exhibition/controller/ExhibitionController.java
@@ -1,14 +1,26 @@
 package com.exhibition.controller;
 
+import com.exhibition.entity.Exhibition;
 import com.exhibition.service.IExService;
 import com.exhibition.service.impl.ExServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Date;
 
 @RestController
 @RequestMapping("/exhibition")
 public class ExhibitionController {
     @Autowired
     private IExService exService = new ExServiceImpl();
+
+    @GetMapping("/{exId}")
+    public Exhibition getExhibitionInfo(@PathVariable Integer exId){
+        Exhibition exInfo = new Exhibition(1,"2","3", Date.valueOf("2001-01-01"),Date.valueOf("2001-03-03"),"Shanghai","sssssssssss\nsssssss","https://bilibili.com");
+        System.out.println(exId);
+        return exInfo;
+    }
 }
--- src/main/java/com/exhibition/entity/Exhibition.java
+++ src/main/java/com/exhibition/entity/Exhibition.java
@@ -3,13 +3,15 @@ package com.exhibition.entity;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import java.util.Date;
+import java.sql.Date;
 
 @Data
 @TableName("exhibition")
+@AllArgsConstructor
 public class Exhibition {
     @TableId
     private Integer id; // 主键id
