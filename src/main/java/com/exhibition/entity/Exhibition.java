68482e31e59a36aa36ed896fe8ae089253ca0494
zyw <78946068+SleepinWei@users.noreply.github.com>
2023-04-17 01:17:10 +0800

    fix : 修复时间字段名称问题

--- src/main/java/com/exhibition/entity/Exhibition.java
+++ src/main/java/com/exhibition/entity/Exhibition.java
@@ -3,12 +3,15 @@ package com.exhibition.entity;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import net.bytebuddy.asm.Advice;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.sql.Date;
+import java.time.LocalDateTime;
 
 @Data
 @TableName("exhibition")
@@ -25,11 +28,13 @@ public class Exhibition {
     private String organizer; // 主办方
 
     @TableField
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
+//    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date begin_date; // 开始日期
 
     @TableField
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
+//    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date end_date; // 结束日期
 
     @TableField
