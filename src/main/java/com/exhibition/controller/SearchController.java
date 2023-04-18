6df5144315acfee6199e12ef3ab2663a3adbf307
SleepinWei <zyw0241@outlook.com>
2023-04-18 16:36:38 +0800

    feat : 合并exhibition相关controller

--- src/main/java/com/exhibition/controller/SearchController.java
+++ src/main/java/com/exhibition/controller/SearchController.java
@@ -5,19 +5,19 @@ import com.exhibition.entity.Exhibition;
 import com.exhibition.entity.derived.ExSearchResult;
 import com.exhibition.service.IExService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
-@RequestMapping
+@RequestMapping()
 public class SearchController {
     @Autowired
     private IExService exService;
 
-    @GetMapping("/search")
-    public ExSearchResult search(@RequestParam(name = "querytext") String querytext){
+    @GetMapping("/searchByKeyword")
+    public ExSearchResult searchByKeyword(@RequestParam(name = "querytext") String querytext){
         Exhibition searchResult = exService.getOne(
                 new QueryWrapper<Exhibition>()
                         .select("name,location,begin_date,end_date")
@@ -29,6 +29,23 @@ public class SearchController {
             searchResult.getBegin_date(),
             searchResult.getEnd_date()
         );
+        return result;
+    }
+
+    @GetMapping("/searchById")
+    public Exhibition searchById(HttpServletRequest request, HttpServletResponse response, @RequestParam(name = "exId") Integer exId){
+        // test
+//        Exhibition exInfo = new Exhibition(1,"2","3", Date.valueOf("2001-01-01"),Date.valueOf("2001-03-03"),"Shanghai","sssssssssss\nsssssss","https://bilibili.com");
+        Exhibition result= exService.getById(exId);
+        if(result == null){
+//            The Exhibition is not found
+            System.out.println("Exhibition Not Exist");
+
+            response.setStatus(400);
+            return new Exhibition();
+        }
+
+        System.out.print(result);
         return result;
     }
 }
