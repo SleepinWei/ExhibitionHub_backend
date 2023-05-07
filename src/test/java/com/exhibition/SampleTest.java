c85cdaa982c803b771c2231cffb9d0d6dc852a7e
daisy <lucylucylucy@163.com>
2023-05-07 15:15:41 +0800

    feat: 修改数据结构，修复筛选后数据丢失问题

--- src/test/java/com/exhibition/SampleTest.java
+++ src/test/java/com/exhibition/SampleTest.java
@@ -1,13 +1,14 @@
 package com.exhibition;
 
 import com.exhibition.controller.CalenderController;
-import com.exhibition.entity.SubExhibition;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @SpringBootTest
@@ -26,9 +27,21 @@ public class SampleTest {
         String ar="";
         String ve="国家典籍";
         String tag="";
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
 
+        for(int i=0;i<list.size();i++){
+            Collections.swap(list,i,list.size()-1);
+            list.remove(list.size()-1);
+            i--;
+            System.out.println(list);
+        }
 
-        List<SubExhibition> ex=controller.calendarSelect(userid,src,dst,ve,tag,pro,ci,ar);
-        System.out.println(ex);
+        //List<SubExhibition> ex=controller.calendarSelect(userid,src,dst,ve,tag,pro,ci,ar);
+        //System.out.println(ex);
     }
 }
