29895991d9fef0fee9506f12cfcb858ddda153a1
SleepinWei <zyw0241@outlook.com>
2023-05-07 16:03:59 +0800

    feat : 修改 Subscription 定义

--- src/main/java/com/exhibition/entity/Subscription.java
+++ src/main/java/com/exhibition/entity/Subscription.java
@@ -3,28 +3,31 @@ package com.exhibition.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.io.Serializable;
+import java.sql.Date;
+
 /**
  * @Author: JudyLou
  * @Date: 2023/4/29 12:10
  */
 @Data
 @TableName("subscription")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Subscription implements Serializable {
     @TableId(value = "user_id", type = IdType.AUTO)
     private Integer user_id; // 订阅某展览的用户id
 
     private Integer ex_id; // 订阅的展览id
 
-    private String date; // 用户选择的观展日期
-
-    public Subscription(){
-    }
-    public Subscription(Integer user_id, Integer ex_id, String date) {
-        this.user_id = user_id;
-        this.ex_id = ex_id;
-        this.date = date;
-    }
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date date; // 用户选择的观展日期
 
 }
