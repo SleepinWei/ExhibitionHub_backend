77fa47367080dd0b3a3ad82184bbf0794d636d67
SleepinWei <zyw0241@outlook.com>
2023-05-13 21:15:21 +0800

    feat : 添加省市区查询

--- src/main/java/com/exhibition/controller/AreaController.java
+++ src/main/java/com/exhibition/controller/AreaController.java
@@ -1,28 +1,33 @@
 package com.exhibition.controller;
 
+import com.exhibition.mapper.AreaMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping
 public class AreaController {
 
+    @Autowired
+    AreaMapper areaMapper;
     @GetMapping("/location/province")
-    List<String> getProvinceList(){
-        return null;
+    List<String> getProvinceList() {
+        return areaMapper.getProvinceList();
     }
 
     @GetMapping("/location/city_list")
-    List<String> getCityList(@RequestParam(name = "province") String province){
-        return null;
+    List<String> getCityList(@RequestParam(name = "province") String province) {
+        return areaMapper.getCityList(province);
     }
 
-    @GetMapping("/location/section_list")
-    List<String> getSectionList(@RequestParam(name="city") String city){
-        return null;
+    @GetMapping("/location/area_list")
+    List<String> getSectionList(@RequestParam(name = "city") String city) {
+        return areaMapper.getSectionList(city);
     }
 }
--- src/main/java/com/exhibition/controller/ExhibitionController.java
+++ src/main/java/com/exhibition/controller/ExhibitionController.java
@@ -60,7 +60,6 @@ public class ExhibitionController {
             return new Exhibition();
         }
 
-        System.out.print(result);
         return result;
     }
 
